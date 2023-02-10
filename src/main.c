0e54df72d840eab1b4b7182cd875d65188d0e7fe
John <linya0o@163.com>
2023-02-10 16:01:25 +0800

    exercises 11

--- src/main.c
+++ src/main.c
@@ -3,43 +3,45 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("ERROR: You need one argument.\n");
-        // this is how you abort a program
-        return 1;
-    }
+    int numbers[4] = { 0 };
+    char name[4] = { 'a' };
 
-    int i = 0;
-    for (i = 0; argv[1][i] != '\0'; i++) {
-        char letter = argv[1][i];
+    // first, print them out raw
+    printf("numbers: %d %d %d %d\n",
+            numbers[0], numbers[1], numbers[2], numbers[3]);
 
-        switch (letter) {
-            case 'a':
-            case 'A':
-                printf("%d: 'A'\n", i); break;
-            case 'e':
-            case 'E':
-                printf("%d: 'E'\n", i); break;
-            case 'i':
-            case 'I':
-                printf("%d: 'I'\n", i); break;
-            case 'o':
-            case 'O':
-                printf("%d: 'O'\n", i); break;
-            case 'u':
-            case 'U':
-                printf("%d: 'U'\n", i); break;
-            case 'y':
-            case 'Y':
-                if (i > 2) {
-                    // it's only sometimes Y
-                    printf("%d: 'Y'\n", i);
-                }
-                break;
-            default:
-                printf("%d: %c is not a vowel\n", i, letter);
-        }
-    }
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1], name[2], name[3]);
+
+    printf("name: %s\n", name);
+
+    // set up the numbers
+    numbers[0] = 1;
+    numbers[1] = 2;
+    numbers[2] = 3;
+    numbers[3] = 4;
+
+    // set up the name
+    name[0] = 'z';
+    name[1] = 'e';
+    name[2] = 'd';
+    name[3] = '\0';
+
+    // then print them out initialized
+    printf("numbers: %d %d %d %d\n",
+            numbers[0], numbers[1], numbers[2], numbers[3]);
+
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1], name[2], name[3]);
+
+    printf("name: %s\n", name);
+
+    // another way to use name
+    char *another = "Zed";
+
+    printf("another: %s\n", another);
+    printf("another each: %c %c %c %c\n",
+            another[0], another[1], another[2], another[3]);
 
     return EXIT_SUCCESS;
 }

69bd2d0f2b4ae26780ecae644929148cdd704337
John <linya0o@163.com>
2023-02-10 15:03:04 +0800

    exercises 9

--- src/main.c
+++ src/main.c
@@ -3,18 +3,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int i = 1;
-    if (argc == 1) {
-        printf("You only have one argument. You suck.\n");
-    } else if (argc > 1 && argc < 4) {
-        printf("Here's your arguments:\n");
-
-        for (i = 0; i < argc; i++) {
-            printf("%s ", argv[i]);
-        }
-        printf("\n");
-    } else {
-        printf("You have too many arguments. You suck.\n");
+    int i = 0;
+    while (i < 25) {
+        printf("%3d", i);
+        i++;
     }
 
     return EXIT_SUCCESS;

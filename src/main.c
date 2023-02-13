453bbec6827bc0d7b64e39e10e6a897a8a9216ac
John <linya0o@163.com>
2023-02-13 15:55:30 +0800

    exercises 16

--- src/main.c
+++ src/main.c
@@ -1,50 +1,72 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
+
+struct Person {
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person* Person_create(char *name,
+        int age, int height, int weight);
+
+void Person_destroy(struct Person *who);
+
+void Person_print(struct Person *who);
 
 int main(int argc, char *argv[]) {
-    // create two arrays we care about
-    int ages[] = { 23, 43, 12, 89, 2 };
-    char *names[] = {
-        "Alan", "Frank",
-        "Mary", "John", "Lisa"
-    };
+    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
 
-    // safely get the size of ages
-    int count = sizeof(ages) / sizeof(int);
-    int i = 0;
+    struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
 
-    // first way using indexing
-    for (i = 0; i < count; i++) {
-        printf("%s has %d years alive.\n", names[i], ages[i]);
-    }
+    // print them out and where they are in memory
+    printf("Joe is at memory location %p:\n", (void *)joe);
+    Person_print(joe);
 
-    printf("---\n");
+    printf("Frank is at memory location %p:\n", (void *)frank);
+    Person_print(frank);
 
-    // setup the pointers to the start of the arrays
-    int *cur_age = ages;
-    char **cur_name = names;
+    // make everyone age 20 years and print them again
+    joe->age += 20;
+    joe->height -= 2;
+    joe->weight += 40;
+    Person_print(joe);
 
-    // second way using pointers
-    for (i = 0; i < count; i++) {
-        printf("%s is %d years old.\n",
-                *(cur_name + i), *(cur_age + i));
-    }
+    frank->age += 20;
+    frank->weight += 20;
+    // free(frank), frank = NULL;
+    Person_print(frank);
 
-    printf("---\n");
-
-    // third way, pointers are just arrays
-    for (i = 0; i < count; i++) {
-        printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
-    }
-
-    printf("---\n");
-
-    // fourth way with pointers in a stupid complex way
-    for (cur_name = names, cur_age = ages;
-            (cur_age - ages) < count; cur_name++, cur_age++) {
-        printf("%s lived %d years so far.\n", *cur_name, *cur_age);
-    }
+    // destroy them both so we clean up
+    Person_destroy(joe);
+    Person_destroy(frank);
 
     return EXIT_SUCCESS;
+}
+
+struct Person* Person_create(char *name,
+        int age, int height, int weight) {
+    struct Person *who = malloc(sizeof(*who));
+    assert(who != NULL);
+
+    *who = (struct Person) { name, age, height, weight };
+
+    return who;
+}
+
+void Person_destroy(struct Person *who) {
+    assert(who != NULL);
+
+    // free(who->name);  // Error: the string is stored at static constant area.
+    free(who);
+}
+
+void Person_print(struct Person *who) {
+    assert(who != NULL);
+
+    printf("Name: %s\n\tAge: %d\n\tHeight: %d\n\tWeight: %d\n",
+            who->name, who->age, who->height, who->weight);
 }
